c00f92675dbdf27090a3b9d3ac1b06c36338ea1b
Li Xueli <mixj93@163.com>
2020-02-04 20:07:25 +0800

    Initial Commit

---
+++ Font Viewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Font Viewer
+//
+//  Created by lixueli on 2020/2/4.
+//  Copyright © 2020 lixueli. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
